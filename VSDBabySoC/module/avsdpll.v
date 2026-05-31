bbf3b08393abd2056d2624a65c18380a679893db
sidj21 <siddarthj2005@gmail.com>
2026-05-31 02:58:02 -0400

    Add: VSDBabySoC - Verilog modules for core files

---
+++ VSDBabySoC/module/avsdpll.v
@@ -0,0 +1,17 @@
+/*Inputs:
+     - VCO_IN, ENb_CP, ENb_VCO, REF: Control and reference signals for PLL operation.
+  Output:
+     - CLK: A stable clock signal for synchronizing the core and other modules.
+*/
+
+module avsdpll(
+    input wire VCO_IN,
+    input wire ENb_CP,
+    input wire ENb_VCO,
+    input wire REF,
+);
+
+    wire CLK;
+
+
+endmodule
